ebd297614a5b1f4459c41817d06de49d8d5f812f
woominKO <woomin422@gmail.com>
2025-03-31 06:40:08 +0900

    ,ㅏ

--- src/main/java/com/example/TeamPle_Self/TeamPleSelfController.java
+++ src/main/java/com/example/TeamPle_Self/TeamPleSelfController.java
@@ -1,4 +1,13 @@
 package com.example.TeamPle_Self;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TeamPleSelfController {
+    private final RoomRepository roomRepository;
+
+    TeamPleSelfController(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
 }
