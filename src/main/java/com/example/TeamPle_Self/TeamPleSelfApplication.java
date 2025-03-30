9f603e0038823e4ea7c2d647e07605944f71bc5c
Woomin Ko <woomin422@gmail.com>
2025-03-30 20:02:03 +0900

    ㅇㅇ

---
+++ src/main/java/com/example/TeamPle_Self/TeamPleSelfApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeamPle_Self;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPleSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamPleSelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TeamPle_Self/TeamPleSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeamPle_Self;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPleSelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
